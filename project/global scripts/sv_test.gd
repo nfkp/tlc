fd3e4f3195a9dc2d6f12704d2f5e8038a1b3b21a
nfkp <108262288+nfkp@users.noreply.github.com>
2025-04-14 16:48:42 -0400

    bicycle 1

--- project/global scripts/sv_test.gd
+++ project/global scripts/sv_test.gd
@@ -1,18 +1,4 @@
 extends SunVoxGD
 
-var time : float = 0.0
-var ok : bool = false
-
 func _ready() -> void:
 	sv_load("res://music/SV_01.sunvox")
-	sv_lock_slot()
-	sv_pattern_mute("M_01", 0)
-	sv_pattern_mute("M_02", 0)
-	sv_unlock_slot()
-	sv_play()
-
-func _process(delta: float) -> void:
-	time += delta
-	if time >= 10. && ok == false:
-		sv_stop()
-		ok = true
