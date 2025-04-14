15448908910046e735c9a62efb1d8a6a5d29ab93
nfkp <108262288+nfkp@users.noreply.github.com>
2025-04-14 16:51:41 -0400

    fix bicycle

--- src/Bicycle/source.cpp
+++ src/Bicycle/source.cpp
@@ -6,15 +6,15 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_bicycle(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 
-	GDREGISTER_RUNTIME_CLASS(SunVoxGD);
+	GDREGISTER_RUNTIME_CLASS(Bicycle);
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
+void uninitialize_bicycle(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -25,8 +25,8 @@ extern "C" {
 GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
 	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-	init_obj.register_initializer(initialize_example_module);
-	init_obj.register_terminator(uninitialize_example_module);
+	init_obj.register_initializer(initialize_bicycle);
+	init_obj.register_terminator(uninitialize_bicycle);
 	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	return init_obj.init();
